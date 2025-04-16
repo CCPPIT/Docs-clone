42a5067a347c9f09ade23e3b3246e90f60968121
unknown <almawisti.cchh@gmail.com>
2025-04-15 20:37:30 -0700

    update1

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,6 +1,4 @@
-
 import React from 'react'
-
 import { Id } from '../../../../convex/_generated/dataModel'
 import { auth } from '@clerk/nextjs/server'
 import { preloadQuery } from 'convex/nextjs'
@@ -8,25 +6,29 @@ import { api } from '../../../../convex/_generated/api'
 import { Document } from './document'
 
 type Props = {
-  params:Promise<{documentId:Id<"documents">}>;
+  params: { documentId: Id<"documents"> }; // التصحيح هنا فقط
 }
 
-export const DocumentIdPage = async({params}: Props) => {
- 
-   const {documentId}=await params;
-   const {getToken}=await auth();
-   const token= await getToken({template:"convex"})?? undefined;
+export default async function DocumentId ({params}: Props) {
+   const {documentId} = await params; // إزالة await هنا
+   const {getToken} = await auth();
+   const token = await getToken({template:"convex"}) ?? undefined;
+   
    if(!token){
+
     throw new Error("Unauthorized")
    }
-   const preloadedDocument=await preloadQuery(
-    api.documents.getById,{id:documentId},
+   
+   const preloadedDocument = await preloadQuery(
+    api.documents.getById,
+    {id:documentId},
     {token}
    )
+   
    if(!preloadedDocument){
     throw new Error("Document not found")
    }
-  return <Document preloadedDocument={preloadedDocument}/>
+   
+   return <Document preloadedDocument={preloadedDocument}/>
 }
 
-export default DocumentIdPage
